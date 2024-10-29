e5f8b8d5cb9d99b4a30f1fcace2a8a7bd901b34c
Krzysztof Jeske <krzysztof.jeske@gmail.com>
2024-10-29 16:04:04 +0100

    New variables

--- templates/tailwind/tailwind.config.js
+++ templates/tailwind/tailwind.config.js
@@ -5,15 +5,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
+        main: {
+          DEFAULT: "hsl(var(--main))",
+          foreground: "hsl(var(--main-foreground))"
         },
         primary: {
           DEFAULT: "hsl(var(--primary))",
@@ -24,19 +18,15 @@ module.exports = {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
         },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
         accent: {
           DEFAULT: "hsl(var(--accent))",
           foreground: "hsl(var(--accent-foreground))",
         },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
+        error: {
+          DEFAULT: "hsl(var(--error))",
+          foreground: "hsl(var(--error-foreground))",
         },
-        border: "hsl(var(--border))",
+        control: "hsl(var(--control))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
       }
